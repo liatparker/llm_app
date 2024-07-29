65453c6b9773e06d4cd71c99225223b32026e445
liatparker <35032879+liatparker@users.noreply.github.com>
2024-07-29 11:52:51 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -11,7 +11,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import PyPDFLoader
 import PyPDF2
 #from langchain import OpenAI, PromptTemplate
-##import glob
+import glob
 
 
 def summarize_pdfs_from_folder(pdf_file):
@@ -21,7 +21,7 @@ def summarize_pdfs_from_folder(pdf_file):
     #summaries = []
     #for pdf_file in pdfs_folder:
     #for pdf_file in glob.glob(pdfs_folder + "/*.pdf"):
-        
+    glob.glob(pdf_file)
     loader = PyPDFLoader(pdf_file)
     docs = loader.load_and_split()
     chain = load_summarize_chain(llm, chain_type="map_reduce")
