ea3ed29649baf224605e48c1a7f752e9acdfa51b
Liat Parker <liatparker@h-MacBook-Air-sl-Liat.local>
2024-07-30 20:30:42 +0300

    update

--- app1.py
+++ app1.py
@@ -1,3 +1,5 @@
+
+
 import streamlit as st
 from langchain_openai import OpenAI
 from langchain.docstore.document import Document
@@ -6,9 +8,59 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.chains.summarize import load_summarize_chain
 #from langchain.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import PyPDFLoader, UnstructuredFileLoader
+import PyPDF2
+#from langchain import OpenAI, PromptTemplate
+import glob
 
 
-def generate_response(txt):
+def summarize_pdfs_from_folder (pdf_file):
+    #uploaded_file = st.file_uploader(
+    #pdf_file, type="pdf")#, accept_multiple_files=True)
+    llm = OpenAI(temperature=0.2, openai_api_key=openai_api_key) 
+    #summaries = []
+    #for pdf_file in pdfs_folder:
+    #for pdf_file in glob.glob(pdfs_folder + "/*.pdf"):
+    #pdf_file = glob.glob(pdf_file)
+    if pdf_file :
+       temp_file = "./temp.pdf"
+       with open(temp_file, "wb") as file:
+        file.write(uploaded_file.getvalue())
+        file_name = pdf_file.name
+
+       loader = UnstructuredFileLoader(temp_file, strategy="fast")
+    #loader = PyPDFLoader(pdf_file)
+       docs = loader.load_and_split()
+       chain = load_summarize_chain(llm, chain_type="map_reduce")
+       summary = chain.run(docs)
+       return summary
+       #st.write(summary)
+    #print("Summary for: ", pdf_file)
+    #print(summary)
+        #print("\n")
+        #summaries.append(summary)
+    #return summary
+    #return summaries
+
+
+def custom_summary(pdf_folder, custom_prompt):
+    summaries = []
+    for pdf_file in glob.glob(pdf_folder + "/*.pdf"):
+        loader = PyPDFLoader(pdf_file)
+        docs = loader.load_and_split()
+        prompt_template = custom_prompt + """
+
+        {text}
+
+        SUMMARY:"""
+        PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+        chain = load_summarize_chain(llm, chain_type="map_reduce",
+                                     map_prompt=PROMPT, combine_prompt=PROMPT)
+        summary_output = chain({"input_documents": docs}, return_only_outputs=True)["output_text"]
+        summaries.append(summary_output)
+
+    return summaries
+
+def generate_response1(txt):
     # Instantiate the LLM model
     llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
     # Split text
@@ -25,16 +77,31 @@ st.set_page_config(page_title='🦜🔗 Text Summarization App')
 st.title('🦜🔗 Text Summarization App')
 
 # Text input
-txt_input = st.text_area('Enter your text', '', height=200)
+
+txt_input = st.text_area('upload your pdf file', '', height=200)
+
+uploaded_file = st.file_uploader(
+    "upload pdf file", type="pdf")#, accept_multiple_files=True)
+#for uploaded_file in uploaded_files:
+#if uploaded_file is not None:
+    # Read the PDF file
+ #   pdf_reader = PyPDF2.PdfReader(uploaded_file)
+    # Extract the content
+ #   content = ""
+ #   for page in range(len(pdf_reader.pages)):
+ #       content += pdf_reader.pages[page].extract_text()
+    # Display the content
+ #   st.write(content)   
 
 # Form to accept user's text input for summarization
 result = []
 with st.form('summarize_form', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
+    openai_api_key = st.text_input('OpenAI API Key', type = 'password')
     submitted = st.form_submit_button('Submit')
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
-            response = generate_response(txt_input)
+            response= summarize_pdfs_from_folder(uploaded_file)
+            #response = generate_response(txt_input )
             result.append(response)
             del openai_api_key
 
