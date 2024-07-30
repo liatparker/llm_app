8ddd25dc1b619dd3fc3436b34b94c97e7b553015
Liat Parker <liatparker@h-MacBook-Air-sl-Liat.local>
2024-07-30 21:16:12 +0300

    update

--- app1.py
+++ app1.py
@@ -8,7 +8,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.chains.summarize import load_summarize_chain
 #from langchain.document_loaders import PyPDFLoader
 from langchain_community.document_loaders.unstructured import UnstructuredFileLoader
-import PyPDF2
+#import PyPDF2
 #from langchain import OpenAI, PromptTemplate
 import glob
 
