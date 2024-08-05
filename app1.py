7f9e886f0ec0fb93b1822825fb2987764ea2b338
Liat Parker <liatparker@h-MacBook-Air-sl-Liat.local>
2024-08-05 21:47:39 +0300

    update

--- app1.py
+++ app1.py
@@ -17,7 +17,7 @@ from langchain_anthropic import AnthropicLLM
 def summarize_pdfs_from_folder (pdf_file):
     #uploaded_file = st.file_uploader(
     #pdf_file, type="pdf")#, accept_multiple_files=True)
-    llm = AnthropicLLM(model='claude-2.1', anthropic_api_key = anthropic_api_key,temperature=0.0)
+    llm = AnthropicLLM(model='claude-2.1', anthropic_api_key = anthropic_api_key,temperature=0.2)
     with open(pdf_file.name, mode='wb') as w:
         w.write(pdf_file.getvalue())
     if pdf_file :  # check if path is not None
@@ -62,7 +62,7 @@ def custom_summary(pdf_folder, custom_prompt):
 
 def generate_response1(txt):
     # Instantiate the LLM model
-    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    llm = OpenAI(temperature=0.2, openai_api_key=openai_api_key)
     # Split text
     text_splitter = CharacterTextSplitter()
     texts = text_splitter.split_text(txt)
