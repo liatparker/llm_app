057a9baafa802dff2041b82e1936dd3a073e4044
Liat Parker <liatparker@h-MacBook-Air-sl-Liat.local>
2024-08-05 21:05:52 +0300

    update

--- app1.py
+++ app1.py
@@ -17,7 +17,7 @@ from langchain_anthropic import AnthropicLLM
 def summarize_pdfs_from_folder (pdf_file):
     #uploaded_file = st.file_uploader(
     #pdf_file, type="pdf")#, accept_multiple_files=True)
-    llm = AnthropicLLM(model='claude-2.1', anthropic_api_key = anthropic_api_key)
+    llm = AnthropicLLM(model='claude-2.1', anthropic_api_key = anthropic_api_key,temperature=0 )
     with open(pdf_file.name, mode='wb') as w:
         w.write(pdf_file.getvalue())
     if pdf_file :  # check if path is not None
