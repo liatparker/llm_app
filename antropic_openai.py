7dd8d1dbbeafde953995b5ff570a60dc9be7478a
Liat Parker <liatparker@gmail.com>
2024-08-20 17:20:16 +0300

    update

--- antropic_openai.py
+++ antropic_openai.py
@@ -75,10 +75,9 @@ MODEL_NAME = 'claude-3-5-sonnet-20240620'
 result = []
 
 if uploaded_file is not None:
-    text = ""
     reader = PdfReader(uploaded_file)
-    for page in reader.pages:
-        text += page.extract_text()
+    text = ''.join(page.extract_text() for page in reader.pages)
+    print(text)
 prompts = [
     f"""Here is an academic paper: <paper>{text}</paper>
 
